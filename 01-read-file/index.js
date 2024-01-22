b81d4fdfc79056eba17e02ab9f967ed9c2a03cb2
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-01-22 15:40:54 +0600

    refactor: remove 'node:' prefix

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { stdout } = require('node:process');
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
 const filePath = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(filePath, 'utf8');
 readStream.on('data', (chunk) => stdout.write(chunk));
